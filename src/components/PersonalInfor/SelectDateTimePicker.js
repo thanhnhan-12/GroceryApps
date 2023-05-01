23c1abbe0ced5c863de3309d1fad35d406350e67
unknown <nguyenthanhnhan12701@gmail.com>
2023-05-01 22:52:28 +0700

    Dropdown list

--- src/components/PersonalInfor/SelectDateTimePicker.js
+++ src/components/PersonalInfor/SelectDateTimePicker.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 const SelectDateTimePicker = () => {
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const [selectedDate, setSelectedDate ] = useState('Chọn ngày tháng năm sinh')
+  const [selectedDate, setSelectedDate] = useState('Chọn ngày tháng năm sinh');
 
   const showDatePicker = () => {
     setDatePickerVisibility(true);
@@ -17,18 +17,20 @@ const SelectDateTimePicker = () => {
   const handleDateConfirm = date => {
     console.warn('A date has been picked: ', date);
     const dt = new Date(date);
-    const x = dt.toISOString().split("T");
-    const x1 = x[0].split("-");
-    console.log(x1[2] + '/' + x1[1] + '/' + x1[0] );
-    setSelectedDate(x1[2] + '/' + x1[1] + '/' + x1[0])
+    const x = dt.toISOString().split('T');
+    const x1 = x[0].split('-');
+    console.log(x1[2] + '/' + x1[1] + '/' + x1[0]);
+    setSelectedDate(x1[2] + '/' + x1[1] + '/' + x1[0]);
     hideDatePicker();
   };
 
   return (
     <View>
-      <Text style={styles.label}>Chọn ngày tháng năm sinh</Text>
+      <Text style={[styles.label, styles.commonInput]}>
+        Chọn ngày tháng năm sinh
+      </Text>
 
-      <TouchableOpacity style={styles.btnLogin} onPress={showDatePicker} >
+      <TouchableOpacity style={styles.input} onPress={showDatePicker}>
         <Text style={styles.textLogin}>{selectedDate}</Text>
       </TouchableOpacity>
 
@@ -43,13 +45,17 @@ const SelectDateTimePicker = () => {
 };
 
 const styles = StyleSheet.create({
+  commonInput: {
+    marginTop: 20,
+  },
+
   label: {
     color: '#333333',
     fontSize: 14,
     fontWeight: 500,
   },
 
-  btnLogin: {
+  input: {
     height: 45,
     marginTop: 6,
     marginRight: 20,
@@ -57,14 +63,11 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 12,
     borderColor: '#ccc',
-    // backgroundColor: '#F2F2F2',
   },
 
   textLogin: {
-    // color: 'white',
     fontWeight: 700,
     width: '100%',
-    textAlign: 'center',
   },
 });
 
